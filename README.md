1feabd4b32ea7c79c5c1f0f72c0c42c447a9cf99
NikkelM <57323886+NikkelM@users.noreply.github.com>
2024-02-11 17:34:46 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# RainbowSixSiegeRandomizer
+ A simple Python script to randomly select bans and operator selections for Rainbow Six: Siege
---
+++ main.py
@@ -0,0 +1,58 @@
+import random
+
+mapStrings = ["FIRST", "SECOND", "THIRD", "FOURTH", "FIFTH"]
+
+attackers = [
+    "Sledge", "Thatcher", "Ash", "Thermite", "Twitch", "Montagne",
+    "Glaz", "Fuze", "Blitz", "IQ", "Buck", "Blackbeard", "Capitao",
+    "Hibana", "Jackal", "Ying", "Zofia", "Dokkaebi", "Lion", "Finka",
+    "Maverick", "Nomad", "Gridlock", "Nokk", "Amaru", "Kali", "Iana",
+    "Ace", "Zero", "Flores", "Osa", "Sens", "Grim", "Brava", "Ram"
+]
+
+defenders = [
+    "Smoke", "Mute", "Castle", "Pulse", "Doc", "Rook", "Kapkan",
+    "Tachanka", "Jäger", "Bandit", "Frost", "Valkyrie", "Caveira",
+    "Echo", "Mira", "Lesion", "Ela", "Vigil", "Alibi", "Maestro",
+    "Clash", "Kaid", "Mozzie", "Warden", "Goyo", "Wamai", "Oryx",
+    "Melusi", "Aruni", "Thunderbird", "Thorn", "Azami", "Solis",
+    "Fenrir", "Tubarao"
+]
+
+if __name__ == "__main__":
+    while True:
+        print(f"Ban the {random.sample(mapStrings, k=1)[0]} map in the selection, and these operators:")
+        print(f"Attack:  {random.sample(attackers, k=2)}")
+        print(f"Defense: {random.sample(defenders, k=2)}")
+        print()
+        print("Enter banned operators, separated by space:")
+
+        print("Attackers")
+        attBans = input().split()
+        for op in attBans:
+            attackers.remove(op) if op in attackers else print(f"Skipping \"{op}\", as it is not a valid attacker.")
+
+        print("Defenders")
+        defBans = input().split()
+        for op in defBans:
+            defenders.remove(op) if op in defenders else print(f"Skipping \"{op}\", as it is not a valid defender.")
+
+        print("Starting rounds...")
+        while True:
+            print("a for attack, d for defense, r to reset map, x to exit:")
+            inp = input()
+            if inp == "a":
+                lis = random.sample(attackers, k=5)
+            elif inp == "d":
+                lis = random.sample(defenders, k=5)
+            elif inp == "r":
+                print("\n\nResetting...\n")
+                break
+            elif inp == "x":
+                print("Exiting...")
+                exit()
+            else:
+                print("Invalid input.")
+                continue
+            for i in range(5):
+                print(f"Player {i}: {lis[i]}")
