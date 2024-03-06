d2e0a3a942436c19deb46ac4ad0f1269af636f42
Nikkel Mollenhauer <57323886+NikkelM@users.noreply.github.com>
2024-03-06 21:26:20 +0000

    Added a `!swap` command to switch the currently played operator (#61)

--- cogs/ongoingMatch.py
+++ cogs/ongoingMatch.py
@@ -1,5 +1,7 @@
 from discord.ext import commands
+from fuzzywuzzy import process
 from bot import RainbowBot
+from rainbow import RainbowData
 
 class OngoingMatch(commands.Cog, name='Ongoing Match'):
     """Commands to interact with an ongoing match, such as banning operators or playing rounds."""
@@ -117,6 +119,51 @@ class OngoingMatch(commands.Cog, name='Ongoing Match'):
             self.bot.saveDiscordMessage(ctx, discordMessage)
             await self._endMatch(ctx)
 
+    @commands.command(aliases=['swap', 'switch', 'newOperator', 'newOp'])
+    async def _swap(self, ctx: commands.Context, player, operator = None):
+        """Swaps the operator a player is playing for another. Use **!swap operator** to swap the operator you are playing, or **!swap @player operator** to swap the operator another player is playing."""
+        match, discordMessage, canContinue = await self.bot.getMatchData(ctx)
+        if not canContinue:
+            return
+        if ctx.message.id != discordMessage['matchMessageId'] or not discordMessage['matchMessageId']:
+            await ctx.message.delete()
+
+        if match.currRound == 0:
+            discordMessage['messageContent']['actionPrompt'] = 'You can only swap operators during an ongoing round. Use **!attack** ⚔️ or **!defense** 🛡️ to start the match.'
+            await self.bot.sendMatchMessage(ctx, discordMessage)
+            return
+        
+        validOperators = RainbowData.attackers if match.playingOnSide == 'attack' else RainbowData.defenders
+
+        # No arguments given
+        if player is None:
+            discordMessage['messageContent']['statsBanner'] = 'You must include the operator you are swapping to. Use **!swap operator** to try again. Use **!swap @player operator** to swap another player\'s operator.'
+        # Only one argument given, it has to be an operator
+        if operator is None:
+            operator = player
+            player = ctx.author
+            if not isinstance(operator, str):
+                discordMessage['messageContent']['statsBanner'] = f'**{operator}** is not a valid operator. Use **!swap operator** to try again. Use **!swap @player operator** to swap another player\'s operator.'
+                await self.bot.sendMatchMessage(ctx, discordMessage)
+                return
+        else:
+            player = await commands.MemberConverter().convert(ctx, player)
+
+        operatorMatch, score = process.extractOne(operator, validOperators)
+        if score >= 75:
+            operator = operatorMatch
+        else:
+            discordMessage['messageContent']['statsBanner'] = f'**{operator}** is not a valid operator. Use **!swap @player operator** to try again.'
+            await self.bot.sendMatchMessage(ctx, discordMessage)
+            return
+
+        playerOperators, backupOperators = match.swapOperator(player, operator)
+
+        discordMessage = self._setRoundLineup(discordMessage, match, playerOperators, backupOperators)
+
+        self.bot.saveOngoingMatch(ctx, match)
+        await self.bot.sendMatchMessage(ctx, discordMessage)
+
     async def _banUnban(self, ctx: commands.Context, *operators, ban=True):
         match, discordMessage, canContinue = await self.bot.getMatchData(ctx)
         if not canContinue:
@@ -191,17 +238,10 @@ class OngoingMatch(commands.Cog, name='Ongoing Match'):
         if match.playingOnSide == 'defense':
             discordMessage['messageContent']['roundMetadata'] += f'\nChoose the **{site}** site.'
 
-        discordMessage['messageContent']['roundLineup'] = ''
-        operators_copy = operators.copy()
-        for player, operator in zip(match.players, operators_copy):
-            discordMessage['messageContent']['roundLineup'] += f'{player["mention"]} plays **{operator}**\n'
-            operators.remove(operator)
-        
-        if operators:
-            discordMessage['messageContent']['roundLineup'] += f'Backup operators: **{", ".join(operators)}**\n'
-
         discordMessage['messageContent']['actionPrompt'] = ''
         discordMessage['reactions'] = [] 
+
+        discordMessage = self._setRoundLineup(discordMessage, match, operators)
 
         if match.currRound != 6:
             discordMessage['messageContent']['actionPrompt'] += 'Use "**!won**" 🇼 or "**!lost**" 🇱 to continue.'
@@ -212,10 +252,6 @@ class OngoingMatch(commands.Cog, name='Ongoing Match'):
         elif match.scores["blue"] == 3:
             discordMessage['messageContent']['actionPrompt'] += 'If you lost, use "**!lost attack**" ⚔️ (or "**!lost defense**" 🛡️) to start overtime on the specified side, otherwise use **!won** 🇼 to end the match.'
             discordMessage['reactions'] += ['🇼', '⚔️', '🛡️']
-
-        # If one of the operators is Caveira, add the interrogation emoji to the message
-        if 'Caveira' in operators_copy:
-            discordMessage['reactions'] += ['🗡️']
 
         self.bot.saveOngoingMatch(ctx, match)
         await self.bot.sendMatchMessage(ctx, discordMessage)
@@ -235,6 +271,25 @@ class OngoingMatch(commands.Cog, name='Ongoing Match'):
         self.bot.saveOngoingMatch(ctx, match)
         self.bot.saveCompletedMatch(ctx, match)
         await self.bot.sendMatchMessage(ctx, discordMessage)
+    
+    def _setRoundLineup(self, discordMessage, match, operators: list, backupOperators: list = None):
+        operatorsCopy = operators.copy()
+        discordMessage['messageContent']['roundLineup'] = ''
+        for player, operator in zip(match.players, operatorsCopy):
+            discordMessage['messageContent']['roundLineup'] += f'{player["mention"]} plays **{operator}**\n'
+            operators.remove(operator)
+
+        if backupOperators is None:
+            backupOperators = operators
+
+        if len(backupOperators) > 0:
+            discordMessage['messageContent']['roundLineup'] += f'Backup operators: **{", ".join(backupOperators)}**. Use **!swap** to switch operator.\n'
+
+        # If one of the operators is Caveira, add the interrogation emoji to the message
+        if 'Caveira' in operatorsCopy:
+            discordMessage['reactions'] += ['🗡️']
+        
+        return discordMessage
 
 async def setup(bot: RainbowBot):
     await bot.add_cog(OngoingMatch(bot))
