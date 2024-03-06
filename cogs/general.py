082b8f3a932ab68241a691f33dcf396c703066a7
NikkelM <57323886+NikkelM@users.noreply.github.com>
2024-03-06 20:04:25 +0000

    Added an `!about` command with more metadata information

--- cogs/general.py
+++ cogs/general.py
@@ -20,10 +20,15 @@ class General(commands.Cog, name='General'):
         discordMessage['matchMessageId'] = None
         await self.bot.sendMatchMessage(ctx, discordMessage)
 
-    @commands.command(name='version')
-    async def _version(self, ctx: commands.Context):
-        """Displays the version of the bot."""
-        await ctx.send(f'RandomSixBot is running on **v{VERSION}**.')
+    @commands.command(aliases=['about', 'version'])
+    async def _about(self, ctx: commands.Context):
+        """Displays useful information concerning the bot."""
+        message = 'The RandomSixBot allows you to randomly select bans, operators and more for *Rainbow Six: Siege*.\n\n'
+        message += 'You can view a list of available commands using **!help**, and learn more about a specific command with **!help <command>**.\n\n'
+        message += f'The bot is currently running on **v{VERSION}**.\n\n'
+        message += 'The bot is created and maintained by [@NikkelM](<https://github.com/NikkelM>), you can find it on [GitHub](<https://github.com/NikkelM/RainbowSixSiegeRandomizer>).\n'
+        message += 'Do you want to say thank you and support the bot? You can do so by [buying me a coffee](<https://ko-fi.com/nikkelm>)!\n\n'
+        await ctx.send(message)
 
 async def setup(bot: RainbowBot):
     await bot.add_cog(General(bot))
